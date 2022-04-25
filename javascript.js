47ba7af8797e0d44bfb8702bc30bfeef7acd9424
Xubuntuwu <uwuxubuntu@gmail.com>
2022-04-25 13:24:19 +0200

    phone corrected still no javascript for password

---
+++ javascript.js
@@ -0,0 +1,31 @@
+/*let firstpassword = document.getElementById("password");
+let secondpassword = document.getElementById("confirmpassword");
+const firstpassdiv = document.getElementsByClassName("firstpassworddiv");
+const secondpassdiv = document.getElementsByClassName("secondpassworddiv");
+
+firstpassword.addEventListener('keyup', passwordEqual)
+secondpassword.addEventListener('keyup', passwordEqual)
+
+
+function passwordEqual() {
+    firstpassword = firstpassword.value;
+    secondpassword = secondpassword.value;
+    if (firstpassword!=secondpassword) {
+        if (firstpassword.classList.contains("error")) {
+            console.log("Hello");
+        }
+        else {
+            firstpassdiv.classList.add("error");
+            secondpassword.classList.add("error");
+            firstpassword.classList.add("error");
+        }
+    }
+    else {
+        if (firstpassword.classList.contains("error")) {
+            firstpassdiv.classList.remove("error");
+            secondpassword.classList.remove("error");
+            firstpassword.classList.remove("error");
+        }
+    }
+}
+*/

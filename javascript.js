1e662b96a18e4d12a2f2afb7cdbae8e62a2bc964
Xubuntuwu <uwuxubuntu@gmail.com>
2022-04-25 23:37:33 +0200

    i think the desktop version pretty much works at this point, i still have to figure out how post/get etc and form buttons work because i dont get why it didnt used to submit the form but now suddenly it does

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 const firstpassword = document.getElementById("password");
 const secondpassword = document.getElementById("confirmpassword");
 const firstpassdiv = document.getElementById("firstpassworddiv");
+const secondpassdiv = document.getElementById("secondpassworddiv");
 firstpassword.addEventListener('keyup', passwordEqual)
 secondpassword.addEventListener('keyup', passwordEqual)
 
@@ -13,15 +14,19 @@ function passwordEqual() {
         }
         else {
             firstpassdiv.classList.add("error");
+            secondpassdiv.classList.add("error");
             secondpassword.classList.add("error");
             firstpassword.classList.add("error");
+            firstpassword.setCustomValidity('Passwords must be the same'); /*found this little helpful nugget thank god*/
         }
     }
     else {
         if (firstpassword.classList.contains("error")){
             firstpassdiv.classList.remove("error");
+            secondpassdiv.classList.remove("error");
             secondpassword.classList.remove("error");
             firstpassword.classList.remove("error");
+            firstpassword.setCustomValidity('');
         }
     }
 }
